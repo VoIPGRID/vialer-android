113f795ab413f2b732a5b100e352830d393452a7
Marco Vellinga <m.vellinga@me.com>
2015-11-13 15:02:04 +0100

    VIALA-330-332: ContactSync and T9 improvements

--- app/src/main/java/com/voipgrid/vialer/contacts/AuthenticationService.java
+++ app/src/main/java/com/voipgrid/vialer/contacts/AuthenticationService.java
@@ -1,27 +1,105 @@
 package com.voipgrid.vialer.contacts;
 
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
+import android.accounts.NetworkErrorException;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 
+/**
+ * Service for authenticating a vialer account for android contact sync.
+ */
 public class AuthenticationService extends Service {
 
-    private static final String TAG = "AuthenticationService";
+    /**
+     * Class for authenticating a vialer account for android contact sync.
+     */
+    private static class Authenticator extends AbstractAccountAuthenticator {
+        private final Context context;
 
-    private Authenticator mAuthenticator;
+        public Authenticator(Context context) {
+            super(context);
+            this.context = context;
+        }
 
-    @Override
-    public void onCreate() {
-        mAuthenticator = new Authenticator(this);
+        @Override
+        public Bundle addAccount(AccountAuthenticatorResponse response, String accountType,
+                                 String authTokenType, String[] requiredFeatures, Bundle options)
+                throws NetworkErrorException {
+            return null;
+        }
+
+        @Override
+        public Bundle getAccountRemovalAllowed(AccountAuthenticatorResponse response,
+                                               Account account)
+                throws NetworkErrorException {
+            return super.getAccountRemovalAllowed(response, account);
+        }
+
+        @Override
+        public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account,
+                                         Bundle options)
+                throws NetworkErrorException {
+            return null;
+        }
+
+        @Override
+        public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+            return null;
+        }
+
+        @Override
+        public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account,
+                                   String authTokenType, Bundle options)
+                throws NetworkErrorException {
+            return null;
+        }
+
+        @Override
+        public String getAuthTokenLabel(String authTokenType) {
+            return null;
+        }
+
+        @Override
+        public Bundle hasFeatures(AccountAuthenticatorResponse response,
+                                  Account account, String[] features)
+                throws NetworkErrorException {
+            return null;
+        }
+
+        @Override
+        public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account,
+                                        String authTokenType, Bundle options)
+                throws NetworkErrorException {
+            return null;
+        }
+
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+    private static Authenticator authenticator = null;
+
+    /**
+     * Getter for the authenticator class.
+     * @return Authenticator object.
+     */
+    protected Authenticator getAuthenticator() {
+        if (authenticator == null) {
+            authenticator = new Authenticator(this);
+        }
+        return authenticator;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return mAuthenticator.getIBinder();
+        if (intent.getAction().equals(AccountManager.ACTION_AUTHENTICATOR_INTENT)) {
+            return getAuthenticator().getIBinder();
+        } else {
+            return null;
+        }
     }
 }
