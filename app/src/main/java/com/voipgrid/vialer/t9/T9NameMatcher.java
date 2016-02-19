9767300370a5f386828dc90e669fe75d8bfab758
Marco Vellinga <m.vellinga@me.com>
2016-02-19 16:44:32 +0100

    VIALA-393: Dialer matched part highlighting

--- app/src/main/java/com/voipgrid/vialer/t9/T9NameMatcher.java
+++ app/src/main/java/com/voipgrid/vialer/t9/T9NameMatcher.java
@@ -29,6 +29,68 @@ public class T9NameMatcher {
     }
 
     /**
+     * Function that surrounds the matched part in the name with <b></b>
+     * @param t9Query The query that is matched on.
+     * @param displayName The name.
+     * @return Name with <b></b> tags.
+     */
+    public static String highlightMatchedPart(String t9Query, String displayName) {
+        ArrayList<String> possibleQueries = T9Query.generateT9NameQueries(displayName);
+        String queryOfWholeName = possibleQueries.get(0);
+
+        String strippedDisplayName = displayName.replaceAll(" ", "");
+
+        int start = queryOfWholeName.indexOf(t9Query);
+        int end = start + t9Query.length();
+
+        // Create a string with the highlighted part without whitespace.
+        StringBuilder builder = new StringBuilder();
+        builder.append(strippedDisplayName.substring(0, start));
+        builder.append("<b>" + strippedDisplayName.substring(start, end) + "</b>");
+        builder.append(strippedDisplayName.substring(end, strippedDisplayName.length()));
+
+        String matchedWithoutSpaces = builder.toString();
+
+        // Clear builder.
+        builder.setLength(0);
+
+        // Loop over indexes of existing spaces.
+        int index = displayName.indexOf(" ");
+        int previousIndex = 0;
+        int count = 0;
+        while (index >= 0) {
+            // Start of the substring.
+            int subStart = previousIndex;
+            int subEnd;
+
+            if (index > (start + count) && index < (end + count)) {
+                // The whitespace is between the <b> tags so add 3 for the first <b>.
+                subEnd = index + 3;
+            } else if (index >= end){
+                // The whitespace is after the <b> tags so add 7 for the <b> and </b>.
+                subEnd = index + 7;
+            } else {
+                // The whitespace is before the <b> tags.
+                subEnd = index;
+            }
+
+            // Subtract the amount of whitespace added.
+            subEnd -= count;
+
+            // Create a substring with trailing whitespace.
+            builder.append(matchedWithoutSpaces.substring(subStart, subEnd) + " ");
+
+            previousIndex = subEnd;
+            count++;
+            index = displayName.indexOf(" ", index + 1);
+        }
+
+        builder.append(matchedWithoutSpaces.substring(previousIndex, matchedWithoutSpaces.length()));
+
+        return builder.toString();
+    }
+
+    /**
      * Custom Comparator to sort the list based on string length DESC.
      */
     private static class T9QueryComparator implements Comparator<String> {
