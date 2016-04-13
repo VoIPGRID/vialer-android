36a83db732a6a85b6463ca138ca5423a2034727e
spindleredmer <redmer.loen@wearespindle.com>
2016-04-13 11:53:08 +0200

    Added support for 64k methods by implementing multidex

--- app/src/main/java/com/voipgrid/vialer/analytics/AnalyticsApplication.java
+++ app/src/main/java/com/voipgrid/vialer/analytics/AnalyticsApplication.java
@@ -1,6 +1,8 @@
 package com.voipgrid.vialer.analytics;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
@@ -13,6 +15,12 @@ import com.voipgrid.vialer.R;
 public class AnalyticsApplication extends Application {
 
     private Tracker mTracker;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     /**
      * Gets the default {@link Tracker} for this {@link Application}.
