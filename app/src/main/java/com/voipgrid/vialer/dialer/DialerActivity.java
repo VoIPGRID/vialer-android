2fc337958dc7165af261d385753dc0b385ba0a3a
stefan strijker <stefan strijker>
2016-07-15 17:05:52 +0200

    VIALA-427: Add support for headset buttons to pickup/hangup or dial

--- app/src/main/java/com/voipgrid/vialer/dialer/DialerActivity.java
+++ app/src/main/java/com/voipgrid/vialer/dialer/DialerActivity.java
@@ -1,6 +1,10 @@
 package com.voipgrid.vialer.dialer;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
@@ -8,6 +12,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -34,6 +39,7 @@ import com.voipgrid.vialer.contacts.SyncUtils;
 import com.voipgrid.vialer.onboarding.SetupActivity;
 import com.voipgrid.vialer.t9.ContactCursorLoader;
 import com.voipgrid.vialer.util.ConnectivityHelper;
+import com.voipgrid.vialer.util.CustomReceiver;
 import com.voipgrid.vialer.util.DialHelper;
 import com.voipgrid.vialer.util.IconHelper;
 import com.voipgrid.vialer.util.LoginRequiredActivity;
@@ -69,6 +75,7 @@ public class DialerActivity extends LoginRequiredActivity implements
     private ConnectivityHelper mConnectivityHelper;
     private JsonStorage mJsonStorage;
     private NetworkStateViewHelper mNetworkStateViewHelper;
+    private BroadcastReceiver mBroadcastReceiver;
 
     private String t9Query;
     private boolean mHasPermission;
@@ -289,6 +296,7 @@ public class DialerActivity extends LoginRequiredActivity implements
     @Override
     protected void onResume() {
         super.onResume();
+        registerReceivers();
 
         // Permission changed since last accessing this Activity.
         if (mHasPermission != ContactsPermission.hasPermission(this)){
@@ -309,6 +317,12 @@ public class DialerActivity extends LoginRequiredActivity implements
 
         mNetworkStateViewHelper.updateNetworkStateView();
         mNetworkStateViewHelper.startListening();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unRegisterReceivers();
     }
 
     @Override
@@ -353,22 +367,50 @@ public class DialerActivity extends LoginRequiredActivity implements
         mNumberInputView.clear();
     }
 
+    private void createReceivers() {
+        mBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                onCallButtonClicked();
+            }
+        };
+    }
+
+    private void registerReceivers() {
+        if (mBroadcastReceiver == null){
+            createReceivers();
+        }
+        ((AudioManager)getSystemService(AUDIO_SERVICE)).registerMediaButtonEventReceiver(
+                new ComponentName(this, CustomReceiver.class));
+        registerReceiver(mBroadcastReceiver, new IntentFilter(CustomReceiver.CALL_BTN));
+    }
+
+    private void unRegisterReceivers() {
+        try {
+            unregisterReceiver(mBroadcastReceiver);
+        } catch(Exception e) {}
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_call :
-                String phoneNumber = mNumberInputView.getNumber();
-                if (phoneNumber != null && !phoneNumber.isEmpty()) {
-                    onCallNumber(PhoneNumberUtils.format(phoneNumber), null);
-                } else {
-                    // Set last dialed number on call button clicked when number is empty.
-                    String last_dialed = mSharedPreferences.getString(LAST_DIALED, "");
-                    mNumberInputView.setNumber(last_dialed);
-                }
+                onCallButtonClicked();
                 break;
             case R.id.button_dialpad :
                 toggleKeyPadView();
                 break;
+        }
+    }
+
+    private void onCallButtonClicked() {
+        String phoneNumber = mNumberInputView.getNumber();
+        if (phoneNumber != null && !phoneNumber.isEmpty()) {
+            onCallNumber(PhoneNumberUtils.format(phoneNumber), null);
+        } else {
+            // Set last dialed number on call button clicked when number is empty.
+            String last_dialed = mSharedPreferences.getString(LAST_DIALED, "");
+            mNumberInputView.setNumber(last_dialed);
         }
     }
 
