09b247a794ea8133618999420280e93ee61564be
jeremy.norman <jeremy.norman@wearespindle.com>
2018-03-05 09:37:18 +0100

    Sending full JSON payload to logentries.

--- app/src/main/java/com/voipgrid/vialer/logging/LogHelper.java
+++ app/src/main/java/com/voipgrid/vialer/logging/LogHelper.java
@@ -1,16 +1,23 @@
 package com.voipgrid.vialer.logging;
 
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.voipgrid.vialer.R;
 import com.voipgrid.vialer.VialerApplication;
 import com.voipgrid.vialer.sip.SipService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LogHelper {
 
     private RemoteLogger mRemoteLogger;
+    private Gson mGson;
 
     private LogHelper(RemoteLogger remoteLogger) {
         mRemoteLogger = remoteLogger;
+        mGson = new GsonBuilder().disableHtmlEscaping().create();
     }
 
     public static LogHelper using(RemoteLogger remoteLogger) {
@@ -44,12 +51,16 @@ public class LogHelper {
      * Sends a log to the push logging environment, used to track success rates of push messages.
      *
      * @param remoteMessage
-     * @param uniqueKey
      */
-    public void logMiddlewareMessageReceived(RemoteMessage remoteMessage, String uniqueKey) {
+    public void logMiddlewareMessageReceived(RemoteMessage remoteMessage) {
+        Map<String, String> message = new HashMap<>(remoteMessage.getData());
+
+        message.put("caller_id", "<CALLER_ID>");
+        message.put("phonenumber", "<PHONE_NUMBER>");
+
         mRemoteLogger.getVialerLogger().logToEnvironment(
                 VialerApplication.get().getString(R.string.push_log_entries_token),
-                "MessageID: " + remoteMessage.getMessageId() + " - UniqueKey: " + uniqueKey
+                "ANDROID : " + mGson.toJson(message)
         );
     }
 }
