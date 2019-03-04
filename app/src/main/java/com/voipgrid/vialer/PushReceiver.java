62f794f08420daf4c487ab8e9146bc0ce640bc7b
Tsoutsouri <stellatsouts@hotmail.com>
2019-03-04 09:09:46 +0100

    cahnges PushReceiver

--- app/src/main/java/com/voipgrid/vialer/PushReceiver.java
+++ app/src/main/java/com/voipgrid/vialer/PushReceiver.java
@@ -1,7 +1,13 @@
 package com.voipgrid.vialer;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import me.pushy.sdk.Pushy;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.content.Context;
@@ -9,36 +15,46 @@ import android.app.PendingIntent;
 import android.media.RingtoneManager;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
+import android.os.Build;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class PushReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        String notificationTitle = "MyApp";
-        String notificationText = "Test notification";
 
-        // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
-        if (intent.getStringExtra("message") != null) {
-            notificationText = intent.getStringExtra("message");
+        public void onReceive(Context context, Intent intent) {
+            Log.e("pushMessageId", "I am here!");
+            // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
+            if (intent.getIntExtra("push-message-id", 0) != 0) {
+                int pushMessageId = intent.getIntExtra("push-message-id",0);
+                Log.e("testJeremy","testJeremy"+pushMessageId);
+                try {
+                    run("http://10.13.25.90/confirm/" + pushMessageId);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
 
-        // Prepare a notification with vibration, sound and lights
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setAutoCancel(true)
-                .setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setContentTitle(notificationTitle)
-                .setContentText(notificationText)
-                .setLights(Color.RED, 1000, 1000)
-                .setVibrate(new long[]{0, 400, 250, 400})
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT));
+        public static void run(String url) throws IOException {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
 
-        // Automatically configure a Notification Channel for devices running Android O+
-        Pushy.setNotificationChannel(builder, context);
+            new OkHttpClient().newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Log.e("failedresponse","The response failed" + e.getMessage());
+                }
 
-        // Get an instance of the NotificationManager service
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-
-        // Build the notification and display it
-        notificationManager.notify(1, builder.build());
+                @Override
+                public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                    Log.e("response","The response is:" +response);
+                }
+            });
+        }
     }
 }
