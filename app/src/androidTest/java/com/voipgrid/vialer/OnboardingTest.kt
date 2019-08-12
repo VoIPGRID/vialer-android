0e4993cd8c91ac03f100eb8acbdca22fc3ace77e
jeremy.norman <jeremy.norman@wearespindle.com>
2019-08-12 12:54:47 +0100

    Place a call to a number and making sure it is answered.

--- app/src/androidTest/java/com/voipgrid/vialer/OnboardingTest.kt
+++ app/src/androidTest/java/com/voipgrid/vialer/OnboardingTest.kt
@@ -1,13 +1,11 @@
 package com.voipgrid.vialer
 
 
-import android.Manifest
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.rule.GrantPermissionRule
 import com.voipgrid.vialer.onboarding.OnboardingActivity
 import org.hamcrest.core.IsNot.not
 import org.junit.Rule
@@ -18,9 +16,6 @@ class OnboardingTest : VialerTest() {
     @Rule
     @JvmField
     var mActivityTestRule = VialerActivityTestRule(OnboardingActivity::class.java, resetStateAfterEachTest = true)
-
-    @Rule @JvmField
-    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.READ_PHONE_STATE, Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_CONTACTS)
 
     @Test
     fun loginFailsWithInvalidCredentials() {
@@ -68,26 +63,6 @@ class OnboardingTest : VialerTest() {
 
     @Test
     fun itSkipsPermissionsAndFinishedOnboarding() {
-        login()
-        enterMobileNumber()
-        onView(withText("Battery Optimization")).check(matches(isDisplayed()))
-        onView(withId(R.id.denyButton)).perform(click())
-        Thread.sleep(5000)
-        onView(withText("MY CALLS")).check(matches(isDisplayed()))
-    }
-
-    private fun enterMobileNumber(number: String = "+447123463890") {
-        onView(withId(R.id.mobileNumberTextDialog)).perform(replaceText(""))
-        onView(withId(R.id.mobileNumberTextDialog)).perform(typeText(number))
-        onView(withId(R.id.button_configure)).check(matches(isEnabled()))
-        onView(withId(R.id.button_configure)).perform(click())
-        Thread.sleep(2000)
-    }
-
-    private fun login(username: String = BuildConfig.TEST_USERNAME, password: String = BuildConfig.TEST_PASSWORD) {
-        onView(withId(R.id.emailTextDialog)).perform(typeText(username))
-        onView(withId(R.id.passwordTextDialog)).perform(typeText(password))
-        onView(withId(R.id.button_login)).perform(click())
-        Thread.sleep(2000)
+        onboard()
     }
 }
--- app/src/androidTest/java/com/voipgrid/vialer/VialerTest.kt
+++ app/src/androidTest/java/com/voipgrid/vialer/VialerTest.kt
@@ -1,10 +1,47 @@
 package com.voipgrid.vialer
 
+import android.Manifest
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
 import androidx.test.runner.AndroidJUnit4
-import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-open class VialerTest
+open class VialerTest {
+
+    @Rule
+    @JvmField
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.READ_PHONE_STATE, Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_CONTACTS)
+
+    protected fun enterMobileNumber(number: String = "+447123463890") {
+        onView(withId(R.id.mobileNumberTextDialog)).perform(replaceText(""))
+        onView(withId(R.id.mobileNumberTextDialog)).perform(typeText(number))
+        onView(withId(R.id.button_configure)).check(matches(ViewMatchers.isEnabled()))
+        onView(withId(R.id.button_configure)).perform(click())
+        Thread.sleep(2000)
+    }
+
+    protected fun login(username: String = BuildConfig.TEST_USERNAME, password: String = BuildConfig.TEST_PASSWORD) {
+        onView(withId(R.id.emailTextDialog)).perform(typeText(username))
+        onView(withId(R.id.passwordTextDialog)).perform(typeText(password))
+        onView(withId(R.id.button_login)).perform(click())
+        Thread.sleep(2000)
+    }
+
+    protected fun onboard() {
+        login()
+        enterMobileNumber()
+        onView(ViewMatchers.withText("Battery Optimization")).check(matches(isDisplayed()))
+        onView(withId(R.id.denyButton)).perform(click())
+        Thread.sleep(5000)
+        onView(ViewMatchers.withText("MY CALLS")).check(matches(isDisplayed()))
+    }
+}
