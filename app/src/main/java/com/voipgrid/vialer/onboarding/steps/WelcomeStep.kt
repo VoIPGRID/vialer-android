8ec78bfec2ca57fedcc5243b9f69216ae0e02afb
jeremy.norman <jeremy.norman@wearespindle.com>
2019-09-09 16:04:43 +0100

    Regularly update voip parameters as opus is now our default codec

--- app/src/main/java/com/voipgrid/vialer/onboarding/steps/WelcomeStep.kt
+++ app/src/main/java/com/voipgrid/vialer/onboarding/steps/WelcomeStep.kt
@@ -2,12 +2,14 @@ package com.voipgrid.vialer.onboarding.steps
 
 import android.os.Bundle
 import android.view.View
+import androidx.work.*
 import com.voipgrid.vialer.R
 import com.voipgrid.vialer.VialerApplication
 import com.voipgrid.vialer.api.models.SystemUser
 import com.voipgrid.vialer.callrecord.importing.HistoricCallRecordsImporter
 import com.voipgrid.vialer.logging.Logger
 import com.voipgrid.vialer.onboarding.core.AutoContinuingStep
+import com.voipgrid.vialer.tasks.launch.UpdateVoipAccountParametersWorker
 import kotlinx.android.synthetic.main.onboarding_step_welcome.*
 
 class WelcomeStep : AutoContinuingStep() {
@@ -22,7 +24,17 @@ class WelcomeStep : AutoContinuingStep() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        activity?.let { HistoricCallRecordsImporter.Worker.start(it) }
+        activity?.let {
+            HistoricCallRecordsImporter.Worker.start(it)
+
+            WorkManager.getInstance(it).enqueueUniqueWork(
+                    UpdateVoipAccountParametersWorker::class.java.name,
+                    ExistingWorkPolicy.KEEP,
+                    OneTimeWorkRequestBuilder<UpdateVoipAccountParametersWorker>()
+                        .setConstraints(Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
+                        .build()
+            )
+        }
         logger.i("Welcome ${user.fullName} to the app and completing onboarding")
         subtitle_label.text = user.fullName
     }
