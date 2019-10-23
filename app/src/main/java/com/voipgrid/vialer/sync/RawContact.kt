ef383047c60e1c9207c0072e5dcbd620855301e2
Yassin Amhagi <amhagi@coffeeit.nl>
2019-10-23 12:55:02 +0200

    Add support for multiple phone number support.

--- app/src/main/java/com/voipgrid/vialer/sync/RawContact.kt
+++ app/src/main/java/com/voipgrid/vialer/sync/RawContact.kt
@@ -2,10 +2,9 @@ package com.voipgrid.vialer.sync
 
 import android.content.Context
 import android.database.Cursor
-import android.provider.ContactsContract.RawContacts
-import android.provider.ContactsContract.Data
-import android.provider.ContactsContract.CommonDataKinds.StructuredName
+import android.provider.ContactsContract.*
 import android.provider.ContactsContract.CommonDataKinds.Phone
+import android.provider.ContactsContract.CommonDataKinds.StructuredName
 import com.voipgrid.vialer.R
 
 data class RawContact(val rawContactId: Long) {
@@ -16,19 +15,19 @@ data class RawContact(val rawContactId: Long) {
     var displayName: String? = null
     var firstName: String? = null
     var lastName: String? = null
-    var mobileNumber: String? = null
+    val phoneNumbers = ArrayList<String>()
 
-    public fun readRawContactData(cursor: Cursor) {
-        contactId = cursor.getLong(cursor.getColumnIndex(RawContacts.CONTACT_ID))
+    public fun readRawContactData(cursor: Cursor, default: Boolean) {
+        contactId = cursor.getLong(cursor.getColumnIndex(if (default) Contacts._ID else RawContacts.CONTACT_ID))
         accountType = cursor.getString(cursor.getColumnIndex(RawContacts.ACCOUNT_TYPE))
     }
 
     public fun readPhoneData(cursor: Cursor) {
         displayName = cursor.getString(cursor.getColumnIndex(Data.DISPLAY_NAME))
-        mobileNumber = cursor.getString(cursor.getColumnIndex(Phone.NUMBER))
+        phoneNumbers.add(cursor.getString(cursor.getColumnIndex(Phone.NUMBER)))
     }
 
-    public fun readGeneralData(cursor: Cursor) {
+    public fun readNameData(cursor: Cursor) {
         firstName = cursor.getString(cursor.getColumnIndex(StructuredName.GIVEN_NAME))
         lastName = cursor.getString(cursor.getColumnIndex(StructuredName.FAMILY_NAME))
     }
