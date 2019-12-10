6c7c3276a0eeed6cea60d90da5e442e937b109d3
Stella Tsoutsouri <stella@wearespindle.com>
2019-12-10 14:55:13 +0100

    Feature/129 require user to fully complete onboarding

--- app/src/main/java/com/voipgrid/vialer/MainActivity.kt
+++ app/src/main/java/com/voipgrid/vialer/MainActivity.kt
@@ -83,7 +83,7 @@ class MainActivity : NavigationDrawerActivity() {
      *
      */
     private fun userIsOnboarded(): Boolean {
-        return User.isLoggedIn && User.voipgridUser?.mobileNumber != null
+        return User.isLoggedIn && User.voipgridUser?.mobileNumber != null && User.internal.hasCompletedOnBoarding
     }
 
     /**
