7ebb1fea528a0808e5663a718f748e6938f51ad0
Tsoutsouri <stella@wearespindle.com>
2019-12-10 14:42:14 +0100

    solved merge conflicts mistake

--- app/src/main/java/com/voipgrid/vialer/persistence/Details.kt
+++ app/src/main/java/com/voipgrid/vialer/persistence/Details.kt
@@ -12,5 +12,13 @@ object Internal : DefaultKotPrefModel() {
     var phoneAccountsCache by gsonNullablePref(PhoneAccountFetcher.PhoneAccountsCache(), PhoneAccountFetcher.PhoneAccountsCache::class.java.name)
     var lastDialledNumber by stringPref(default = "")
     var callRecordMonthsImported by gsonPref(mutableListOf<DateTime>())
+    var destinations by gsonPref(listOf<UserDestination>(), "destinations")
+
+    val phoneNumbers: List<String>
+        get() = destinations.map { it.internalNumber }.toMutableList().plus(destinations.flatMap { it.phoneAccounts }.map { it.number })
+
+    val phoneAccounts: List<String>
+        get() = destinations.flatMap { it.phoneAccounts }.map { it.id }
+
     var hasCompletedOnBoarding by booleanPref(default = true)
 }
