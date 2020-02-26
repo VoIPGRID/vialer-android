e63a3c442f9049df04c8c8042253ceb01fc29ecc
Stella Tsoutsouri <stella@wearespindle.com>
2020-02-26 13:56:20 +0100

    fcm messaging service refactoring

--- app/src/main/java/com/voipgrid/vialer/koin/AppModule.kt
+++ app/src/main/java/com/voipgrid/vialer/koin/AppModule.kt
@@ -1,11 +1,15 @@
 package com.voipgrid.vialer.koin
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.os.PowerManager
 import android.telephony.TelephonyManager
 import com.voipgrid.vialer.api.SecureCalling
 import com.voipgrid.vialer.api.ServiceGenerator
 import com.voipgrid.vialer.api.UserSynchronizer
+import com.voipgrid.vialer.call.NativeCallManager
 import com.voipgrid.vialer.util.BatteryOptimizationManager
+import com.voipgrid.vialer.util.ConnectivityHelper
 import com.voipgrid.vialer.util.Sim
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -21,6 +25,12 @@ val appModule = module {
     single { BatteryOptimizationManager(androidContext()) }
 
     single { androidContext().getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager }
+
+    single { ConnectivityHelper(get(),get()) }
+    single { androidContext().getSystemService(Context.POWER_SERVICE) as PowerManager }
+    single { NativeCallManager(get()) }
+    single { ServiceGenerator.createRegistrationService(androidContext()) }
+    single { androidContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
 
     single { Sim(get()) }
 }
--- app/src/main/java/com/voipgrid/vialer/tasks/launch/RegisterLibraries.kt
+++ app/src/main/java/com/voipgrid/vialer/tasks/launch/RegisterLibraries.kt
@@ -6,9 +6,6 @@ import com.chibatching.kotpref.gsonpref.gson
 import com.github.anrwatchdog.ANRWatchDog
 import com.github.tamir7.contacts.Contacts
 import com.google.gson.GsonBuilder
-import com.segment.analytics.Analytics
-import com.segment.analytics.Analytics.setSingletonInstance
-import com.voipgrid.vialer.R
 import com.voipgrid.vialer.VialerApplication
 import com.voipgrid.vialer.koin.appModule
 import org.koin.android.ext.koin.androidContext
@@ -25,7 +22,7 @@ class RegisterLibraries : OnLaunchTask {
     override fun execute(application: VialerApplication) {
         ANRWatchDog().start()
         Contacts.initialize(application)
-        initializeSegmentAnalytics(application)
+       initializeSegmentAnalytics(application)
         Kotpref.gson = GsonBuilder().create()
 
         startKoin {
